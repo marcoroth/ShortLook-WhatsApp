b4b53f4fb8fea23fa886f332cd2991058300fcd3
Marco Roth <marco.roth@intergga.ch>
2018-07-20 18:30:10 +0200

    change profile picture source and add group chat support
    Co-Authored-By: Gil Shahar <gilshahardex99@gmail.com>

--- WhatsAppContactPhotoProvider.m
+++ WhatsAppContactPhotoProvider.m
@@ -1,32 +1,43 @@
+#import "FolderFinder.h"
 #import "WhatsAppContactPhotoProvider.h"
 
 @interface NCNotificationRequest
   -(NSString *)threadIdentifier;
 @end
 
-@interface FBApplicationInfo
-  -(NSURL *)dataContainerURL;
-@end
-
-@interface LSApplicationProxy
-  +(id)applicationProxyForIdentifier:(id)arg1;
-@end
-
 @implementation WhatsAppContactPhotoProvider
   - (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification {
-    FBApplicationInfo *appinfo = [LSApplicationProxy applicationProxyForIdentifier:@"net.whatsapp.WhatsApp"];
     NCNotificationRequest *request = [notification request];
     NSString *threadId = [request threadIdentifier];
-    NSString *phoneNumber = [threadId componentsSeparatedByString:@"@"][0];
-    NSMutableString *imageURL = [NSMutableString new];
-    NSString *containerURL = [[appinfo dataContainerURL] absoluteString];
+    NSString *chatId = [threadId componentsSeparatedByString:@"@"][0];
+    NSString* containerURL = [FolderFinder findSharedFolder:@"group.net.whatsapp.WhatsApp.shared"];
+    containerURL = [NSString stringWithFormat:@"%@/Media/Profile", containerURL];
 
-    [imageURL appendString:containerURL];
-    [imageURL appendString:@"/Library/Caches/spotlight-profile-v2/"];
-    [imageURL appendString:phoneNumber];
-    [imageURL appendString:@"@s-whatsapp-net.png"];
-    [imageURL setString:[imageURL stringByReplacingOccurrencesOfString:@"file://" withString:@""]];
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSDirectoryEnumerator *files = [fileManager enumeratorAtPath:containerURL];
 
+    NSString *file;
+    NSString *profilePicture;
+
+    while ((file = [files nextObject])) {
+      NSArray *parts = [file componentsSeparatedByString:@"-"];
+
+      // DMs
+      if ([parts count] == 2){
+        if ([chatId isEqualToString:parts[0]]){
+          profilePicture = file;
+        }
+      }
+
+      // Groups
+      if ([parts count] == 3){
+        if ([chatId isEqualToString:[NSString stringWithFormat:@"%@-%@", parts[0], parts[1]]]){
+          profilePicture = file;
+        }
+      }
+    }
+
+    NSString *imageURL = [NSString stringWithFormat:@"%@/%@", containerURL, profilePicture];
     UIImage *image = [UIImage imageWithContentsOfFile:imageURL];
 
     return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerInstantlyResolvingPromiseWithPhotoIdentifier:imageURL image:image];
